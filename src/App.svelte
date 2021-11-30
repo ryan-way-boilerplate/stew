5ecb15825d26ce5bedc4b3136f01dcc095bf164b
ryan-way <ryan.keith.way@gmail.com>
2021-11-30 08:37:12 -0800

    testing live reload

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
 	export let name: string;
+  export let ryan: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+  <h1>Hello {ryan}</h1>
+	<p>isit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
